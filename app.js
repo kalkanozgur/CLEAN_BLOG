2579378621bee1c91a2b7114d8b6aed9d5f24393
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-07-27 19:55:25 +0300

    ejs&partials&routes

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs')
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 //MIDDLEWARES
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname + '/public')));
 
+//ROUTES
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
-	
-});
-
+    res.redirect('index')
+})
+app.get('/index', (req, res) => {
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+app.get('/post', (req, res) => {
+    res.render('post')
+})
 const port = 3000;
 app.listen(port, () => {
 	console.log(`Sunucu ${port} portunda başlatıldı`);
