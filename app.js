0d2b20c6d8baeb238b1fdbbc35296193b76da7dc
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-07-28 18:25:20 +0300

    v3için add_post+db kismi yapildi

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const path = require('path');
-const ejs = require('ejs')
+const ejs = require('ejs');
+const mongoose = require('mongoose')
+
+
+const Post = require('./models/post');
 
 const app = express();
 
 app.set('view engine', 'ejs')
 
+//DB Connection
+mongoose
+  .connect('mongodb://localhost/cleanblog-test-db')
+  .then(() => console.log('DB Connection is done'));
+
+
 //MIDDLEWARES
 app.use(express.static(path.resolve(__dirname + '/public')));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.redirect('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    console.log(posts)
+    res.render('index', {posts});
 })
 app.get('/index', (req, res) => {
     res.render('index')
@@ -21,6 +35,10 @@ app.get('/about', (req, res) => {
 })
 app.get('/add_post', (req, res) => {
     res.render('add_post')
+})
+app.post('/add_post', async (req, res) => {
+    await Post.create(req.body)
+    res.redirect('/')
 })
 app.get('/post', (req, res) => {
     res.render('post')
